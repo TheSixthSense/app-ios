269130b58ddace4f00a23c1f4e512424a4360551
Moon Hyo Jae <followmoonrise@gmail.com>
2022-06-27 22:50:16 +0900

    [Core] 프로젝트를 생성하였어요 (#2)

---
+++ SixthSense/SixthSense/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  SixthSense
+//
+//  Created by 문효재 on 2022/06/27.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+}
+
