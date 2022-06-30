eaf19c417e673f39e683cb959cbb8919120cd7c1
Moon Hyo Jae <followmoonrise@gmail.com>
2022-06-30 23:36:28 +0900

    feat :  Window에 RootViewController를 넣는 로직을 추가하였어요

--- SixthSense/SixthSense/Sources/App/AppDelegate.swift
+++ SixthSense/SixthSense/Sources/App/AppDelegate.swift
@@ -6,12 +6,24 @@
 //
 
 import UIKit
+import Then
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+  
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+
     configureDebug()
+    // TODO: 해당로직은 Netfox 화면 나오는지 확인하기 위해서 임시로 추가한 코드입니다. 지우셔도됩니다
+    let window = UIWindow(frame: UIScreen.main.bounds).with {
+      $0.rootViewController = UIViewController()
+      $0.backgroundColor = .white
+      $0.makeKeyAndVisible()
+    }
+    self.window = window
+    
     return true
   }
 }
