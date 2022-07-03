6ad67b3128974f5b14bbd666ebe0beef7a5f9ced
AllieKim <allie0147@gmail.com>
2022-07-03 18:37:57 +0900

    Feat : UserUseCase를 만들었어요.

---
+++ SixthSense/SixthSense/Sources/User/Domain/UseCases/UserUseCase.swift
@@ -0,0 +1,26 @@
+//
+//  UserUseCases.swift
+//  SixthSense
+//
+//  Created by Allie Kim on 2022/07/02.
+//  Copyright © 2022 kr.co.thesixthsense. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+protocol UserUseCaseable {
+//    func fetch() -> Single<String>?
+}
+
+final class UserUseCase: UserUseCaseable {
+
+//    private let userRepository: UserRepository
+//
+//    init(userRepository: UserRepository) {
+//        self.userRepository = userRepository
+//    }
+//
+//    func fetch() -> Single<String>? {
+//    }
+}
