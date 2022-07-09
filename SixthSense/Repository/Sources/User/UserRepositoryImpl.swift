e569619d5db4b42af4af58ace674aed294c5e994
Moon Hyo Jae <followmoonrise@gmail.com>
2022-07-09 23:45:56 +0900

    refactor : `Repository`와 `Network` 로직들을 `Repository` 프로젝트 내부로 고립시켰어요

--- SixthSense/Repository/Sources/User/UserRepositoryImpl.swift
+++ SixthSense/Repository/Sources/User/UserRepositoryImpl.swift
@@ -8,14 +8,14 @@
 
 import RxSwift
 
-final class UserRepositoryImpl: UserRepository {
+public final class UserRepositoryImpl: UserRepository {
   private let network: Network
   
   public init(network: Network) {
     self.network = network
   }
   
-  func user() -> Single<String> {
+  public func user() -> Single<String> {
     return network.request(UserAPI.user)
       .mapString()
       .flatMap { data -> Single<String> in
