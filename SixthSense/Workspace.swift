26cddbe029dc906796dc82bd74eb8a8e4f91301c
Moon Hyo Jae <followmoonrise@gmail.com>
2022-07-09 21:35:48 +0900

    feat : workspace를 구현하였어요

---
+++ SixthSense/Workspace.swift
@@ -0,0 +1,15 @@
+//
+//  Workspace.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 문효재 on 2022/07/09.
+//
+
+import ProjectDescription
+
+let workspace = Workspace(
+    name: "SixthSense",
+    projects: [
+        "VegannerApp", "Storage"
+    ]
+)
