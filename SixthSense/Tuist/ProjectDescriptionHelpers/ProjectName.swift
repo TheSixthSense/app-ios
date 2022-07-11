6b4865e47341f7fc1707d3aa0edf4064720ceab0
Moon Hyo Jae <followmoonrise@gmail.com>
2022-07-11 23:13:07 +0900

    feat : `Project`의 이름과 경로, 프로젝트를 반환하는  `Module` enum을 만들었어요

---
+++ SixthSense/Tuist/ProjectDescriptionHelpers/ProjectName.swift
@@ -0,0 +1,42 @@
+//
+//  ProjectName.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 문효재 on 2022/07/11.
+//
+
+import ProjectDescription
+
+public enum Module {
+  case app
+  
+  // Repository|DataStore
+  case repository
+  case storage
+  
+  // Features
+  
+  // Utils
+  case utils
+}
+
+extension Module {
+  public var name: String {
+    switch self {
+      case .app: return "VegannerApp"
+      case .repository: return "Repository"
+      case .storage: return "Storage"
+      case .utils: return "Utils"
+    }
+  }
+  
+  public var path: ProjectDescription.Path {
+    return .relativeToRoot(self.name)
+  }
+  
+  public var project: TargetDependency {
+    return .project(target: self.name, path: self.path)
+  }
+}
+
+extension Module: CaseIterable { }
