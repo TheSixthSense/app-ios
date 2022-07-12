a8ae9e9b45652d423714c72146477c6ad669f524
Allie Kim <allie0147@gmail.com>
2022-07-12 23:18:28 +0900

    Feat : `AppButton`, `SelectButton` 을 만들었어요

---
+++ SixthSense/DesignSystem/Sources/Buttons/AppButton.swift
@@ -0,0 +1,52 @@
+//
+//  AppButton.swift
+//  DesignSystem
+//
+//  Created by Allie Kim on 2022/07/12.
+//  Copyright © 2022 kr.co.thesixthsense. All rights reserved.
+//
+
+import UIKit
+
+public final class AppButton: UIButton, ButtonProtocol {
+
+    /// .touchUpInside event때 사용
+    public var hasFocused: Bool = false {
+        willSet {
+            backgroundColor = newValue ? .main : .systemGray100
+            titleColor = newValue ? .white : .systemGray300
+        }
+    }
+
+    public var titleText: String {
+        return titleLabel?.text ?? ""
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    public convenience init(title: String) {
+        self.init(frame: .zero)
+        setButtonTitle(with: title)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    var titleColor: AppColor = .systemGray500 {
+        willSet {
+            setTitleColor(newValue, for: .normal)
+        }
+    }
+
+    func setButtonTitle(with string: String) {
+        setAttributedTitle(NSAttributedString(
+            string: string,
+            attributes: [.font: AppFont.body1Bold]
+        ), for: .normal)
+        setTitleColor(titleColor, for: .normal)
+    }
+
+}
