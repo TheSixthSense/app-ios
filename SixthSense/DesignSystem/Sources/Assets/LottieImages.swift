c905e343b2092da738a5174d3280dfd490622619
Moon Hyo Jae <followmoonrise@gmail.com>
2022-07-17 22:40:23 +0900

    feat : `DesignSystem`에 로티 의존을 추가하고 테스트용 로티파일을 추가하였어요

---
+++ SixthSense/DesignSystem/Sources/Assets/LottieImages.swift
@@ -0,0 +1,13 @@
+//
+//  LottieImages.swift
+//  DesignSystem
+//
+//  Created by 문효재 on 2022/07/16.
+//  Copyright © 2022 kr.co.thesixthsense. All rights reserved.
+//
+
+import Lottie
+
+public extension Animation {
+    static let logo: Animation? = .named("temp_logo", bundle: .module)
+}
