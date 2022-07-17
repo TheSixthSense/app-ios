0566cd671e151266ca7d0959bf6d1ba6591943c5
AllieKim <allie0147@gmail.com>
2022-07-17 20:39:57 +0900

    Feat: SignUp UI 구성

--- SixthSense/VegannerApp/Sources/SignUp/SignUpViewController.swift
+++ SixthSense/VegannerApp/Sources/SignUp/SignUpViewController.swift
@@ -6,26 +6,70 @@
 //  Copyright © 2022 kr.co.thesixthsense. All rights reserved.
 //
 
-import RIBs
-import RxSwift
-import UIKit
 import DesignSystem
+import RIBs
+import RxAppState
+import RxSwift
+import RxCocoa
+import RxKeyboard
+import Then
+import UIKit
+
+enum SignUpStep {
+    case one
+    case two
+    case three
+    case four
+}
 
 protocol SignUpPresentableListener: AnyObject {
 }
 
 final class SignUpViewController: UIViewController, SignUpPresentable, SignUpViewControllable {
 
-    weak var listener: SignUpPresentableListener?
+    func changeButtonState(_ state: Bool) {
+        bottomButton.hasFocused = state
+    }
 
-    private var signUpContainerView: UIView
+    weak var listener: SignUpPresentableListener?
 
     private var signUpPageView: SignUpPageViewController
 
+    private lazy var stepIconImageView = UIImageView().then { view in
+        view.image = UIImage(asset: VegannerAppDevAsset.iconStep01)
+        view.contentMode = .scaleAspectFit
+        view.clipsToBounds = true
+    }
+
+    private lazy var backButton = UIButton().then { button in
+        button.setImage(VegannerAppDevAsset.backButton.image, for: .normal)
+    }
+
+    private lazy var navigationTitle = AppLabel().then { label in
+        label.setText("step 1", font: AppFont.body1)
+    }
+
+    private lazy var stepNavigationBar = UIView().then { view in
+        view.backgroundColor = .white
+        view.addSubviews([backButton, navigationTitle])
+    }
+
+    private lazy var stepProgressBar = UIProgressView().then { view in
+        view.progress = 0.25
+        view.progressTintColor = .main
+        view.progressViewStyle = .default
+    }
+
+    private lazy var stepProgressLabel = AppLabel().then { label in
+        label.setText("25%", font: AppFont.caption)
+        label.textColor = .main
+    }
+
     private lazy var bottomButton = AppButton(title: "다음")
 
+    private let disposeBag = DisposeBag()
+
     init() {
-        signUpContainerView = UIView()
         signUpPageView = SignUpPageViewController()
         super.init(nibName: nil, bundle: nil)
     }
@@ -33,95 +77,161 @@ final class SignUpViewController: UIViewController, SignUpPresentable, SignUpVie
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+        
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.addSubviews([signUpContainerView,
+        configureUI()
+        bindUI()
+
+        NotificationCenter.default.rx
+            .notification(.bottomButtonState)
+            .asDriver(onErrorDriveWith: .empty())
+            .drive(onNext: { noti in
+
+            let state = noti.object as? Bool ?? false
+            self.bottomButton.hasFocused = state
+
+        }).disposed(by: disposeBag)
+    }
+}
+
+extension SignUpViewController {
+
+    private func configureUI() {
+        navigationController?.navigationBar.isHidden = true
+
+        view.backgroundColor = .white
+
+        view.addSubviews([stepNavigationBar,
+                          stepProgressBar,
+                          stepProgressLabel,
+                          stepIconImageView,
                           signUpPageView.view,
                           bottomButton])
 
-        signUpContainerView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+        signUpPageView.didMove(toParent: self)
+    }
+
+    private func configureLayout() {
+
+        stepNavigationBar.snp.makeConstraints { make in
+            make.left.right.top.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(44)
+        }
+
+        backButton.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(16)
+            make.bottom.equalToSuperview().inset(10)
+            make.size.equalTo(24)
+        }
+
+        navigationTitle.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalTo(backButton)
+        }
+
+        stepProgressBar.snp.makeConstraints { make in
+            make.left.right.equalToSuperview()
+            make.top.equalTo(stepNavigationBar.snp.bottom)
+            make.height.equalTo(6)
+        }
+
+        stepProgressLabel.snp.makeConstraints { make in
+            make.top.equalTo(stepProgressBar.snp.bottom).offset(2)
+        }
+
+        stepIconImageView.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(32)
+            make.top.equalTo(stepProgressBar.snp.bottom).offset(40)
         }
 
         signUpPageView.view.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+            make.top.equalTo(stepIconImageView.snp.bottom).offset(24)
+            make.left.right.bottom.equalToSuperview()
         }
 
         bottomButton.snp.makeConstraints { make in
             make.left.right.bottom.equalToSuperview()
-            make.height.equalTo(44)
+            make.height.equalTo(view.safeAreaInsets.bottom + 44)
         }
+    }
 
-        signUpPageView.didMove(toParent: self)
+    private func bindUI() {
+        rx.viewDidLayoutSubviews
+            .take(1)
+            .bind(onNext: {
+            self.configureLayout()
+        }).disposed(by: disposeBag)
+
+        signUpPageView.stepDrvier
+            .drive(
+            onNext: { [weak self] step in
+                self?.stepChanged(step)
+            }).disposed(by: disposeBag)
+
+        updateBottomButton()
+
+    }
+
+    private func updateBottomButton() {
+        getKeyboardHeight { [weak self] height in
+            guard let self = self else { return }
+
+            let bottomSafeArea = self.view.safeAreaInsets.bottom
+
+            if height > 0 {
+                self.bottomButton.snp.remakeConstraints { make in
+                    make.left.right.equalToSuperview()
+                    make.bottom.equalTo(self.view.safeAreaLayoutGuide).offset(-height + bottomSafeArea)
+                    make.height.equalTo(44)
+                }
+            } else {
+                self.bottomButton.snp.remakeConstraints { make in
+                    make.left.right.equalToSuperview()
+                    make.bottom.equalToSuperview()
+                    make.height.equalTo(bottomSafeArea + 44)
+                }
+            }
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    private func getKeyboardHeight(completion: @escaping (_ height: CGFloat) -> Void) {
+        RxKeyboard.instance
+            .visibleHeight
+            .skip(1)
+            .drive(onNext: { height in
+            completion(height)
+        }).disposed(by: disposeBag)
+    }
+
+    private func stepChanged(_ step: SignUpStep) {
+        switch step {
+        case .one:
+            navigationTitle.text = "step 1"
+            stepIconImageView.image = UIImage(asset: VegannerAppDevAsset.iconStep01)
+            stepProgressBar.setProgress(0.25, animated: true)
+            stepProgressLabel.text = "25%"
+        case .two:
+            navigationTitle.text = "step 2"
+            stepIconImageView.image = UIImage(asset: VegannerAppDevAsset.iconStep02)
+            stepProgressBar.setProgress(0.5, animated: true)
+            stepProgressLabel.text = "50%"
+        case .three:
+            navigationTitle.text = "step 3"
+            stepIconImageView.image = UIImage(asset: VegannerAppDevAsset.iconStep03)
+            stepProgressBar.setProgress(0.75, animated: true)
+            stepProgressLabel.text = "75%"
+        case .four:
+            navigationTitle.text = "step 4"
+            stepIconImageView.image = UIImage(asset: VegannerAppDevAsset.iconStep04)
+            stepProgressBar.setProgress(1.0, animated: true)
+            stepProgressLabel.text = "100%"
+        }
     }
 }
 
-class SignUpPageViewController: UIPageViewController {
-
-    private lazy var pageViews: [UIViewController] = {
-        var subViews = [UIViewController]()
-        subViews.append(contentsOf: [
-            SignUpFirstStepVIewController(),
-            SignUpSecondStepVIewController(),
-            SignUpThirdStepVIewController(),
-            SignUpLastStepVIewController()
-        ])
-        return subViews
-    }()
-
-    override init(transitionStyle style: UIPageViewController.TransitionStyle, navigationOrientation: UIPageViewController.NavigationOrientation, options: [UIPageViewController.OptionsKey: Any]? = nil) {
-        super.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
-    }
-
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        dataSource = self
-        delegate = nil
-
-        setViewControllers([pageViews[0]],
-                           direction: .forward,
-                           animated: false,
-                           completion: nil)
-    }
-}
-
-extension SignUpPageViewController: UIPageViewControllerDataSource {
-
-    func pageViewController(
-        _ pageViewController: UIPageViewController,
-        viewControllerBefore viewController: UIViewController
-    ) -> UIViewController? {
-
-        guard let viewControllerIndex = pageViews.firstIndex(of: viewController) else { return nil }
-
-        let previousIndex = viewControllerIndex - 1
-
-        guard previousIndex >= 0 else { return nil }
-
-        guard pageViews.count > previousIndex else { return nil }
-
-        return pageViews[previousIndex]
-    }
-
-    func pageViewController(
-        _ pageViewController: UIPageViewController,
-        viewControllerAfter viewController: UIViewController
-    ) -> UIViewController? {
-        guard let viewControllerIndex = pageViews.firstIndex(of: viewController) else { return nil }
-
-        let nextIndex = viewControllerIndex + 1
-
-        guard nextIndex < pageViews.count else { return nil }
-
-        guard pageViews.count > nextIndex else { return nil }
-
-        return pageViews[nextIndex]
-    }
+extension Notification.Name {
+    static let bottomButtonState = NSNotification.Name("signUpBottomButtonState")
 }
--- SixthSense/VegannerApp/Sources/SignUp/SubViews/SignUpThirdStepViewController.swift
+++ SixthSense/VegannerApp/Sources/SignUp/SubViews/SignUpThirdStepViewController.swift
@@ -7,13 +7,107 @@
 //
 
 import UIKit
+import RxSwift
+import Then
+import DesignSystem
 
-final class SignUpThirdStepVIewController: UIViewController {
+final class SignUpThirdStepViewController: UIViewController {
 
+    private var stepLabel = AppLabel().then { label in
+        label.setText("비거너!\n너의 생일을 알려줘", font: AppFont.title2)
+    }
+
+    private var birthStackView = UIStackView().then { stackView in
+        stackView.spacing = 8
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+    }
+
+    private var yearTextField = AppTextField().then { textfield in
+        textfield.placeholderString = "1990"
+        textfield.maxLength = 4
+        textfield.keyboardType = .decimalPad
+    }
+
+    private var yearLabel = AppLabel().then { label in
+        label.setText("년", font: AppFont.body1)
+    }
+
+    private var monthTextField = AppTextField().then { textfield in
+        textfield.placeholderString = "01"
+        textfield.maxLength = 2
+        textfield.keyboardType = .decimalPad
+    }
+
+    private var monthLabel = AppLabel().then { label in
+        label.setText("월", font: AppFont.body1)
+    }
+
+    private var dayTextField = AppTextField().then { textfield in
+        textfield.placeholderString = "01"
+        textfield.maxLength = 2
+        textfield.keyboardType = .decimalPad
+    }
+
+    private var dayLabel = AppLabel().then { label in
+        label.setText("일", font: AppFont.body1)
+    }
+
+    private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
 
-        view.backgroundColor = .green
+        // TODO: - 버튼 활성화 observer
+        Observable.combineLatest([yearTextField.rx.text, monthTextField.rx.text, dayTextField.rx.text])
+            .distinctUntilChanged()
+            .bind { [weak self] text in
+            print("text~ \(text)")
+            NotificationCenter.default.post(name: .bottomButtonState, object: true)
+        }.disposed(by: disposeBag)
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        configureLayout()
+    }
+}
+private extension SignUpThirdStepViewController {
+
+    private func configureUI() {
+        view.addSubviews([stepLabel, birthStackView])
+        birthStackView.addArrangedSubviews([yearTextField,
+                                            yearLabel,
+                                            monthTextField,
+                                            monthLabel,
+                                            dayTextField,
+                                            dayLabel])
+    }
+
+
+    private func configureLayout() {
+        stepLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(24)
+            make.left.right.equalToSuperview().inset(32)
+        }
+
+        birthStackView.snp.makeConstraints { make in
+            make.top.equalTo(stepLabel.snp.bottom).offset(24)
+            make.left.right.equalTo(stepLabel)
+            make.height.equalTo(44)
+        }
+
+        yearTextField.snp.makeConstraints { make in
+            make.width.equalTo(100)
+        }
+
+        monthTextField.snp.makeConstraints { make in
+            make.width.equalTo(60)
+        }
+
+        dayTextField.snp.makeConstraints { make in
+            make.width.equalTo(60)
+        }
     }
 }
