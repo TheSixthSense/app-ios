57e30f32f8421ba183f11504a6358d435366b4cf
HyoJae Moon <followmoonrise@gmail.com>
2022-07-22 00:34:33 +0900

    feat : UIView Extension 모듈을 추가하였어요

---
+++ SixthSense/Splash/Sources/Extensions/UIStackView.swift
@@ -0,0 +1,20 @@
+//
+//  UIStackView+.swift
+//  SixthSense
+//
+//  Created by Allie Kim on 2022/07/07.
+//  Copyright © 2022 kr.co.thesixthsense. All rights reserved.
+//
+
+import UIKit
+
+public extension UIStackView {
+    
+    /// add muliple views into UIStackView
+    func addArrangedSubviews(_ views: [UIView]) {
+        views.forEach { view in
+            self.addArrangedSubview(view)
+        }
+    }
+}
+
---
+++ SixthSense/Splash/Sources/Extensions/UIView+.swift
@@ -0,0 +1,19 @@
+//
+//  View+.swift
+//  SixthSense
+//
+//  Created by Allie Kim on 2022/07/07.
+//  Copyright © 2022 kr.co.thesixthsense. All rights reserved.
+//
+
+import UIKit
+
+public extension UIView {
+    
+    /// add muliple views into UIView
+    func addSubviews(_ views: [UIView]) {
+        views.forEach { view in
+            self.addSubview(view)
+        }
+    }
+}
