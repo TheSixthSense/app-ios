1484285740d5523c302645adf490b99250ac9460
AllieKim <allie0147@gmail.com>
2022-07-22 04:06:48 +0900

    Feat : SignUp에 쓰일 enum들 추가

---
+++ SixthSense/Account/Sources/SignUp/Presentation/Enums/Gender.swift
@@ -0,0 +1,16 @@
+//
+//  Gender.swift
+//  Account
+//
+//  Created by Allie Kim on 2022/07/19.
+//  Copyright © 2022 kr.co.thesixthsense. All rights reserved.
+//
+
+import Foundation
+
+enum Gender: String {
+    case male = "MALE"
+    case female = "FEMALE"
+    case etc = "ETC"
+    case none = "NONE"
+}
---
+++ SixthSense/Account/Sources/SignUp/Presentation/Enums/VeganStage.swift
@@ -0,0 +1,16 @@
+//
+//  VeganStage.swift
+//  Account
+//
+//  Created by Allie Kim on 2022/07/19.
+//  Copyright © 2022 kr.co.thesixthsense. All rights reserved.
+//
+
+import Foundation
+
+enum VeganStage: String {
+    case beginner = "BEGINNER"
+    case junior = "JUNIOR"
+    case senior = "SENIOR"
+    case retry = "RETRY"
+}
