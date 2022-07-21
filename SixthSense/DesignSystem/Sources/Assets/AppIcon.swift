401dda757a9754c4b245ef00add1461e27879621
AllieKim <allie0147@gmail.com>
2022-07-22 04:01:27 +0900

    Feat : 아이콘 추가

--- SixthSense/DesignSystem/Sources/Assets/AppIcon.swift
+++ SixthSense/DesignSystem/Sources/Assets/AppIcon.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2022 kr.co.thesixthsense. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public enum AppIcon {
+
+    public static var back: UIImage {
+        return UIImage(.back)!.color(.systemGray300)
+    }
+
+    public static var close: UIImage {
+        return UIImage(.xmark)!.color(.systemGray300)
+    }
+}
+
+enum SFSymbolKey: String {
+    case back = "chevron.left"
+    case xmark
+}
+
+extension UIImage {
+
+    func color(_ color: AppColor) -> UIImage {
+        return self.withTintColor(color, renderingMode: .alwaysOriginal)
+    }
+
+    convenience init?(_ sfSymbolKey: SFSymbolKey) {
+        self.init(systemName: sfSymbolKey.rawValue)
+    }
+}
--- SixthSense/DesignSystem/Sources/Assets/AppImage.swift
+++ SixthSense/DesignSystem/Sources/Assets/AppImage.swift
@@ -1,5 +1,5 @@
 //
-//  AppIcon.swift
+//  AppImage.swift
 //  DesignSystem
 //
 //  Created by Allie Kim on 2022/07/18.
@@ -9,14 +9,3 @@
 import UIKit
 
 public typealias AppImage = DesignSystemAsset
-
-enum SFSymbolKey: String {
-    case back
-
-}
-
-extension UIImage {
-    convenience init?(_ sfSymbolKey: SFSymbolKey) {
-        self.init(systemName: sfSymbolKey.rawValue)
-    }
-}
