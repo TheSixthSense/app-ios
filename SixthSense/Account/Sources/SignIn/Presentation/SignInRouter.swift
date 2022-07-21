b47fdd7585e662e2f647f0d6dc85737d44c4679f
AllieKim <allie0147@gmail.com>
2022-07-22 04:08:16 +0900

    Feat : `SignInRIB`에 `SignUpRIB` 연결

--- SixthSense/Account/Sources/SignIn/Presentation/SignInRouter.swift
+++ SixthSense/Account/Sources/SignIn/Presentation/SignInRouter.swift
@@ -8,18 +8,34 @@
 
 import RIBs
 
-protocol SignInInteractable: Interactable {
+protocol SignInInteractable: Interactable, SignUpListener {
     var router: SignInRouting? { get set }
     var listener: SignInListener? { get set }
 }
 
 protocol SignInViewControllable: ViewControllable {
-    
 }
 
 final class SignInRouter: ViewableRouter<SignInInteractable, SignInViewControllable>, SignInRouting {
-    override init(interactor: SignInInteractable, viewController: SignInViewControllable) {
+
+    private let signUpBuilder: SignUpBuildable
+
+    private var childRouting: ViewableRouting?
+
+    init(interactor: SignInInteractable, viewController: SignInViewControllable, signUpBuilder: SignUpBuildable) {
+        self.signUpBuilder = signUpBuilder
         super.init(interactor: interactor, viewController: viewController)
         interactor.router = self
+    }
+
+    func routeToSignUp() {
+        if childRouting != nil { return }
+
+        let router = signUpBuilder.build(withListener: self.interactor)
+        let viewController = router.viewControllable
+        viewController.uiviewController.modalPresentationStyle = .fullScreen
+        viewControllable.present(viewController, animated: false)
+        attachChild(router)
+        self.childRouting = router
     }
 }
