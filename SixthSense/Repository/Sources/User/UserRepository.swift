2399c2bd5f1fe9695bea8581ed286818eabe93c6
Allie Kim <allie0147@gmail.com>
2022-07-29 18:54:27 +0900

    Feat : signup, nickname중복체크 api 추가

--- SixthSense/Repository/Sources/User/UserRepository.swift
+++ SixthSense/Repository/Sources/User/UserRepository.swift
@@ -9,6 +9,8 @@
 import RxSwift
 
 public protocol UserRepository: AnyObject {
-  func user() -> Single<String>
-  func login(request: LoginRequest) -> Single<Void>
+    func user() -> Single<String>
+    func login(request: LoginRequest) -> Single<Void>
+    func validateNickname(request: String) -> Single<String>
+    func signUp(request: SignUpRequest) -> Single<String>
 }
