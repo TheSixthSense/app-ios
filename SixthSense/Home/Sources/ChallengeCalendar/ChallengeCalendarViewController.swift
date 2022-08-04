9e99c013908823a0e29bf2919462b7d31202f244
Moon Hyo Jae <followmoonrise@gmail.com>
2022-08-04 23:55:43 +0900

    feat : 챌린지 캘린더 화면에 헤더, 요일, 날짜들을 구성하였어요

--- SixthSense/Home/Sources/ChallengeCalendar/ChallengeCalendarViewController.swift
+++ SixthSense/Home/Sources/ChallengeCalendar/ChallengeCalendarViewController.swift
@@ -9,6 +9,8 @@
 import RIBs
 import RxSwift
 import UIKit
+import DesignSystem
+import JTAppleCalendar
 
 protocol ChallengeCalendarPresentableListener: AnyObject { }
 
@@ -24,14 +26,45 @@ final class ChallengeCalendarViewController: UIViewController, ChallengeCalendar
         }
     }
     
-    private let titleLabel = UILabel().then {
-        $0.text = "캘린더뷰"
-        $0.textColor = UIColor.gray
-        $0.textAlignment = .center
-        $0.numberOfLines = 2
-        $0.sizeToFit()
+    private let headerView = CalendarHeaderView().then {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    private let weekdayView = CalendarWeekdayView().then {
+        $0.translatesAutoresizingMaskIntoConstraints = false
     }
     
+    private let stackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 0
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.alignment = .fill
+        $0.distribution = .fill
+    }
+    
+    private lazy var calendar = JTACMonthView(frame: .zero).then {
+        $0.backgroundColor = .white
+        $0.calendarDataSource = self
+        $0.calendarDelegate = self
+
+        $0.scrollDirection = .horizontal
+        $0.scrollingMode = .stopAtEachCalendarFrame
+        $0.showsHorizontalScrollIndicator = false
+
+        $0.register(CalendarDayCell.self)
+        
+        $0.minimumLineSpacing = 0
+        $0.minimumInteritemSpacing = 0
+
+        $0.isMultipleTouchEnabled = false
+        
+        $0.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    private let bottomLineView = UIView().then {
+        $0.backgroundColor = .systemGray300
+    }
+
     init() {
         super.init(nibName: nil, bundle: nil)
         configureViews()
@@ -44,17 +77,74 @@ final class ChallengeCalendarViewController: UIViewController, ChallengeCalendar
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        configureViews()
+        configureConstraints()
     }
     
     private func configureViews() {
-        view.addSubviews(titleLabel)
+        view.addSubviews(stackView, bottomLineView)
+        stackView.addArrangedSubviews(headerView, weekdayView, calendar)
+    }
+
+    private func configureConstraints() {
+        headerView.snp.makeConstraints {
+            $0.height.equalTo(Constants.Views.headerHeight)
+        }
+        
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16))
+        }
+        
+        calendar.snp.makeConstraints {
+            $0.height.equalTo(370)
+        }
+        
+        bottomLineView.snp.makeConstraints {
+            $0.top.equalTo(calendar.snp.bottom)
+            $0.left.right.bottom.equalToSuperview()
+            $0.height.equalTo(1)
+        }
+    }
+}
+
+extension ChallengeCalendarViewController: JTACMonthViewDataSource {
+    func configureCalendar(_ calendar: JTACMonthView) -> ConfigurationParameters {
+        .init(startDate: "2022-08-01".toDate()!,
+              endDate: "2022-08-31".toDate()!)
+    }
+}
+
+extension ChallengeCalendarViewController: JTACMonthViewDelegate {
+    func calendar(_ calendar: JTACMonthView, willDisplay cell: JTACDayCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        guard let cell = calendar.dequeueReusableJTAppleCell(CalendarDayCell.self, for: indexPath) as? CalendarDayCell else { return }
+        cell.configure(state: cellState)
     }
     
-    private func configureConstraints() {
-        titleLabel.snp.makeConstraints {
-            $0.edges.equalToSuperview()
-            $0.height.equalTo(Constants.Views.headerHeight + Constants.Views.contentsHeight)
+    func calendar(_ calendar: JTACMonthView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTACDayCell {
+        guard let cell = calendar.dequeueReusableJTAppleCell(CalendarDayCell.self, for: indexPath) as? CalendarDayCell else { return .init() }
+        cell.configure(state: cellState)
+        return cell
+    }
+}
+
+extension String {
+    func toDate() -> Date? { //"yyyy-MM-dd HH:mm:ss"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.timeZone = TimeZone(identifier: "KST")
+        if let date = dateFormatter.date(from: self) {
+            return date
+        } else {
+            return nil
         }
+    }
+}
+
+extension Date {
+    func toString() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.timeZone = TimeZone(identifier: "KST")
+        return dateFormatter.string(from: self)
     }
 }
