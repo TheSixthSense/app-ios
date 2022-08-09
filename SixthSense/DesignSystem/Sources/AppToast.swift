be0c6522abea1635ddf6f4235837dff89b050e6a
AllieKim <allie0147@gmail.com>
2022-08-09 23:31:37 +0900

    Feat : Toast에 PanGesture 적용, 코드 정리

--- SixthSense/DesignSystem/Sources/AppToast.swift
+++ SixthSense/DesignSystem/Sources/AppToast.swift
@@ -8,19 +8,40 @@
 
 import UIKit
 
-public enum ToastType {
-    case notice
-    case error
+public enum Toast {
+
+    public enum Style {
+        case notice
+        case error
+    }
+
+    public enum View {
+        static let tag: Int = 999
+    }
 }
 
-final class AppToast: UILabel {
+public final class AppToast: UILabel {
 
-    init(type toastType: ToastType) {
-        let window = UIApplication.shared.windows.first!
-        super.init(frame: CGRect(x: 20,
+    private enum Constants {
+
+        enum Frame {
+            static let x: CGFloat = 20.0
+            static let initalY: CGFloat = UIScreen.main.bounds.height * 0.15
+            static let changedY: CGFloat = -40.0
+            static let width: CGFloat = UIScreen.main.bounds.width - 40
+            static let height: CGFloat = 48.0
+            static let inset: CGFloat = 20.0
+        }
+
+        static let animation: CGFloat = 0.2
+        static let radius: CGFloat = 24.0
+    }
+
+    init(type toastType: Toast.Style) {
+        super.init(frame: CGRect(x: Constants.Frame.x,
                                  y: 0,
-                                 width: window.frame.width - 40,
-                                 height: 48))
+                                 width: Constants.Frame.width,
+                                 height: Constants.Frame.height))
         configureUI(style: toastType)
     }
 
@@ -28,29 +49,25 @@ final class AppToast: UILabel {
         fatalError("init(coder:) has not been implemented")
     }
 
-    override func drawText(in rect: CGRect) {
-        super.drawText(in: rect.inset(by: UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)))
+    public override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: UIEdgeInsets(top: 0, left: Constants.Frame.inset, bottom: 0, right: Constants.Frame.inset)))
     }
 
 
     func showToast(_ message: String, duration: Double = 2.0) {
-        self.attributedText = NSAttributedString(
-            string: message,
-            attributes: [.font: AppFont.body1Bold]
-        )
+        setTextLabel(message)
 
-        let window = UIApplication.shared.windows.first!
-        UIView.animate(withDuration: 0.2, animations: {
-            self.frame = CGRect(x: 20,
-                                y: window.frame.height * 0.15,
+        UIView.animate(withDuration: Constants.animation, animations: {
+            self.frame = CGRect(x: Constants.Frame.x,
+                                y: Constants.Frame.initalY,
                                 width: self.frame.width,
                                 height: self.frame.height)
         }, completion: { (isCompleted) in
             if isCompleted {
                 DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
-                    UIView.animate(withDuration: 0.2, animations: {
-                        self.frame = CGRect(x: 20,
-                                            y: 0,
+                    UIView.animate(withDuration: Constants.animation, animations: {
+                        self.frame = CGRect(x: Constants.Frame.x,
+                                            y: Constants.Frame.changedY,
                                             width: self.frame.width,
                                             height: self.frame.height)
 
@@ -62,12 +79,41 @@ final class AppToast: UILabel {
         })
     }
 
-    private func configureUI(style toastStyle: ToastType) {
+    private func configureUI(style toastStyle: Toast.Style) {
+        self.tag = Toast.View.tag
         self.backgroundColor = toastStyle == .notice ? AppColor.systemBlack.withAlphaComponent(0.9) : AppColor.red500.withAlphaComponent(0.9)
         self.textColor = UIColor.white
         self.textAlignment = .center
-        self.layer.cornerRadius = 24
+        self.layer.cornerRadius = Constants.radius
         self.clipsToBounds = true
+        self.isUserInteractionEnabled = true
+        let swipe = UIPanGestureRecognizer(target: self, action: #selector(toastSwiped))
+        self.addGestureRecognizer(swipe)
+    }
+
+    private func setTextLabel(_ message: String) {
+        self.attributedText = NSAttributedString(
+            string: message,
+            attributes: [.font: AppFont.body1Bold]
+        )
+    }
+
+    @objc
+    private func toastSwiped(_ gesture: UIPanGestureRecognizer) {
+        let minVelocity: CGFloat = 80.0
+        if gesture.state == .began {
+            if abs(gesture.velocity(in: self).y) > minVelocity {
+                UIView.animate(withDuration: 0.1, animations: {
+                    self.frame = CGRect(x: Constants.Frame.x,
+                                        y: Constants.Frame.changedY,
+                                        width: self.frame.width,
+                                        height: self.frame.height)
+
+                }, completion: { _ in
+                    self.removeFromSuperview()
+                })
+            }
+        }
     }
 }
 
