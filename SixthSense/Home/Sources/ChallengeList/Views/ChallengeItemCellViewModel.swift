075f014f5359aadf58395149789850303eeda446
Moon Hyo Jae <followmoonrise@gmail.com>
2022-08-16 23:04:55 +0900

    feat : 챌린지 리스트 아이템 셀에서 사용하기 위한 ViewModel을 작성했어요

---
+++ SixthSense/Home/Sources/ChallengeList/Views/ChallengeItemCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ChallengeItemCellViewModel.swift
+//  Home
+//
+//  Created by 문효재 on 2022/08/16.
+//  Copyright © 2022 kr.co.thesixthsense. All rights reserved.
+//
+
+import Foundation
+
+struct ChallengeItemCellViewModel {
+    let emoji: String
+    let title: String
+}
