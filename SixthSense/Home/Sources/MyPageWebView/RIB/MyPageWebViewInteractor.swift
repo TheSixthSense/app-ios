1c894cc9897cc0a994526c1fc49b4d65b11aea29
AllieKim <allie0147@gmail.com>
2022-09-07 00:29:38 +0900

    Feat : 웹뷰 interactor 구현

---
+++ SixthSense/Home/Sources/MyPageWebView/RIB/MyPageWebViewInteractor.swift
@@ -0,0 +1,47 @@
+//
+//  MyPageWebViewInteractor.swift
+//  Home
+//
+//  Created by Allie Kim on 2022/09/06.
+//  Copyright © 2022 kr.co.thesixthsense. All rights reserved.
+//
+
+import RIBs
+import RxSwift
+
+protocol MyPageWebViewRouting: ViewableRouting {
+}
+
+protocol MyPageWebViewPresentable: Presentable {
+    var listener: MyPageWebViewPresentableListener? { get set }
+}
+
+protocol MypageWebViewPresenterAction: AnyObject {
+    var didTapBackButton: Observable<Void> { get }
+}
+
+protocol MyPageWebViewListener: AnyObject {
+    func pop()
+}
+
+final class MyPageWebViewInteractor: PresentableInteractor<MyPageWebViewPresentable>, MyPageWebViewInteractable, MyPageWebViewPresentableListener {
+    weak var router: MyPageWebViewRouting?
+    weak var listener: MyPageWebViewListener?
+
+    override init(presenter: MyPageWebViewPresentable) {
+        super.init(presenter: presenter)
+        presenter.listener = self
+    }
+
+    override func didBecomeActive() {
+        super.didBecomeActive()
+    }
+
+    override func willResignActive() {
+        super.willResignActive()
+    }
+
+    func pop() {
+        listener?.pop()
+    }
+}
