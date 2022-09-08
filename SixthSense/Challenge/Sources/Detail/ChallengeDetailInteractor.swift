6055c0cd7cee82f9c233b361d4946fbb971ed7db
HyoJae Moon <69489688+MOON-HJ@users.noreply.github.com>
2022-09-08 15:34:40 +0900

    [App] 챌린지 인증 | 보일러플레이트 코드를 추가하고 화면을 구성했어요 (#115)
    * feat : 챌린지인증 | RIB 보일러플레이트 코드를 추가했어요
    
    * feat : 챌린지인증 | 챌린지 인증화면 상단바를 구성하고 닫기 버튼을 구현했어요
    
    * feat : 챌린지인증 | Action을 정의하였어요
    
    * feat : 챌린지인증 | 챌린지 인증 화면 View를 구성하였어요

---
+++ SixthSense/Challenge/Sources/Detail/ChallengeDetailInteractor.swift
@@ -0,0 +1,81 @@
+//
+//  ChallengeDetailInteractor.swift
+//  Challenge_Challenge
+//
+//  Created by 문효재 on 2022/09/07.
+//  Copyright © 2022 kr.co.thesixthsense. All rights reserved.
+//
+
+import RIBs
+import RxSwift
+import RxRelay
+import UIKit
+
+public protocol ChallengeDetailRouting: ViewableRouting { }
+
+protocol ChallengeDetailPresenterAction: AnyObject {
+    var closeDidTap: Observable<Void> { get }
+}
+
+protocol ChallengeDetailPresenterHandler: AnyObject {
+    var image: Observable<UIImage> { get }
+    var date: Observable<Date> { get }
+    var comment: Observable<String?> { get }
+}
+
+protocol ChallengeDetailPresentable: Presentable {
+    var handler: ChallengeDetailPresenterHandler? { get set }
+    var action: ChallengeDetailPresenterAction? { get set }
+}
+
+public protocol ChallengeDetailListener: AnyObject {
+    func detailDidTapClose()
+}
+
+protocol ChallengeDetailInteractorDependency { }
+
+final class ChallengeDetailInteractor: PresentableInteractor<ChallengeDetailPresentable>,
+                                        ChallengeDetailInteractable {
+
+    weak var router: ChallengeDetailRouting?
+    weak var listener: ChallengeDetailListener?
+    private let dependency: ChallengeDetailInteractorDependency
+    
+    private let imageRelay: PublishRelay<UIImage> = .init()
+    private let dateRelay: PublishRelay<Date> = .init()
+    private let commentRelay: PublishRelay<String?> = .init()
+    
+    init(presenter: ChallengeDetailPresentable,
+         dependency: ChallengeDetailInteractorDependency) {
+        self.dependency = dependency
+        super.init(presenter: presenter)
+        presenter.handler = self
+    }
+
+    override func didBecomeActive() {
+        super.didBecomeActive()
+        bind()
+    }
+    
+    private func bind() {
+        guard let action = presenter.action else { return }
+        
+        action.closeDidTap
+            .withUnretained(self)
+            .subscribe(onNext: { owner, _ in
+                owner.listener?.detailDidTapClose()
+            })
+            .disposeOnDeactivate(interactor: self)
+    }
+
+    override func willResignActive() {
+        super.willResignActive()
+    }
+}
+
+// MARK: - Handler
+extension ChallengeDetailInteractor: ChallengeDetailPresenterHandler {
+    var image: Observable<UIImage> { imageRelay.asObservable() }
+    var date: Observable<Date> { dateRelay.asObservable() }
+    var comment: Observable<String?> { commentRelay.asObservable() }
+}
---
+++ SixthSense/Challenge/Sources/Detail/ChallengeDetailViewController.swift
@@ -0,0 +1,161 @@
+//
+//  ChallengeDetailViewController.swift
+//  Challenge_Challenge
+//
+//  Created by 문효재 on 2022/09/07.
+//  Copyright © 2022 kr.co.thesixthsense. All rights reserved.
+//
+
+import RIBs
+import RxSwift
+import UIKit
+import SnapKit
+import Then
+import DesignSystem
+import Foundation
+
+final class ChallengeDetailViewController: UIViewController, ChallengeDetailPresentable, ChallengeDetailViewControllable {
+    private let disposeBag = DisposeBag()
+    weak var handler: ChallengeDetailPresenterHandler?
+    weak var action: ChallengeDetailPresenterAction?
+    
+    private enum Constants {
+        static let dateTransform: (Date) -> String = {
+            DateFormatter().then {
+                $0.dateFormat = "yyyy.MM.dd(E)"
+                $0.timeZone = TimeZone(identifier: "KST")
+                $0.locale = Locale(identifier:"ko_KR")
+            }.string(from: $0)
+        }
+    }
+    
+    private let headerView = UIView()
+    private let titleLabel = UILabel().then {
+        $0.text = "챌린지 인증"
+        $0.font = AppFont.body1Bold
+        $0.textColor = AppColor.systemBlack
+        $0.numberOfLines = 1
+        $0.sizeToFit()
+    }
+    private let closeButton = UIButton().then {
+        $0.setImage(AppIcon.close, for: .normal)
+        $0.setTitleColor(.systemGray500, for: .normal)
+    }
+    
+    private let contentsView = UIView()
+    private let scrollView = UIScrollView().then {
+        $0.showsVerticalScrollIndicator = false
+    }
+    private let stackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 0
+        $0.alignment = .fill
+        $0.distribution = .fill
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    private let imageView = UIImageView().then {
+        $0.backgroundColor = .systemGray300
+        $0.layer.cornerRadius = 30
+        $0.clipsToBounds = true
+    }
+    private let dateLabel = UILabel().then {
+        $0.font = AppFont.body1
+        $0.textColor = .black.withAlphaComponent(0.4)
+        $0.numberOfLines = 1
+        $0.sizeToFit()
+    }
+    private let commentLabel = UILabel().then {
+        $0.font = AppFont.body2
+        $0.textColor = .black
+        $0.numberOfLines = 0
+        $0.sizeToFit()
+    }
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        action = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureViews()
+        configureConstraints()
+        bind()
+    }
+    
+    private func configureViews() {
+        view.backgroundColor = .white
+        view.addSubviews(headerView, contentsView)
+        contentsView.addSubviews(scrollView)
+        scrollView.addSubviews(stackView)
+        headerView.addSubviews(titleLabel, closeButton)
+        stackView.addArrangedSubviews(imageView, dateLabel, commentLabel)
+        stackView.do {
+            $0.setCustomSpacing(10, after: imageView)
+            $0.setCustomSpacing(24, after: dateLabel)
+        }
+    }
+    
+    private func configureConstraints() {
+        headerView.snp.makeConstraints {
+            $0.top.left.right.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(44)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        
+        closeButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.right.equalToSuperview().inset(16)
+        }
+        
+        contentsView.snp.makeConstraints {
+            $0.top.equalTo(headerView.snp.bottom).offset(24)
+            $0.left.right.equalToSuperview().inset(20)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-32)
+        }
+        
+        scrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+
+        stackView.snp.makeConstraints {
+            $0.width.equalToSuperview()
+            $0.edges.equalTo(scrollView.contentLayoutGuide.snp.edges)
+            $0.height.equalTo(scrollView.frameLayoutGuide.snp.height).priority(1)
+        }
+        
+        imageView.snp.makeConstraints {
+            $0.height.equalTo(imageView.snp.width)
+        }
+    }
+    
+    private func bind() {
+        guard let handler = handler else { return }
+        
+        handler.image
+            .bind(to: imageView.rx.image)
+            .disposed(by: self.disposeBag)
+        
+        handler.date
+            .map(Constants.dateTransform)
+            .bind(to: dateLabel.rx.text)
+            .disposed(by: self.disposeBag)
+        
+        handler.comment
+            .bind(to: commentLabel.rx.text)
+            .disposed(by: self.disposeBag)
+    }
+}
+
+// MARK: - Action
+extension ChallengeDetailViewController: ChallengeDetailPresenterAction {
+    var closeDidTap: Observable<Void> { closeButton.rx.tap.asObservable() }
+}
