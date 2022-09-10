86a38ea01024fd06fb7e49fc7e946c0d20f94a44
AllieKim <allie0147@gmail.com>
2022-09-10 15:41:16 +0900

    Feat : 백버튼 액션 추가

--- SixthSense/Home/Sources/MyPageModifyInfo/RIB/MyPageModifyInfoInteractor.swift
+++ SixthSense/Home/Sources/MyPageModifyInfo/RIB/MyPageModifyInfoInteractor.swift
@@ -7,15 +7,29 @@
 //
 
 import RIBs
+import RxRelay
 import RxSwift
 
 protocol MyPageModifyInfoRouting: ViewableRouting {
 
 }
+
 protocol MyPageModifyInfoPresentable: Presentable {
+    var handler: MyPageModifyInfoPresenterHandler? { get set }
+    var action: MyPageModifyInfoPresenterAction? { get set }
+}
+
+protocol MyPageModifyInfoPresenterHandler: AnyObject {
 
 }
+
+protocol MyPageModifyInfoPresenterAction: AnyObject {
+    var didTapBackButton: Observable<Void> { get }
+
+}
+
 protocol MyPageModifyInfoListener: AnyObject {
+    func popModifyInfoView()
 }
 
 final class MyPageModifyInfoInteractor: PresentableInteractor<MyPageModifyInfoPresentable>, MyPageModifyInfoInteractable {
@@ -25,13 +39,32 @@ final class MyPageModifyInfoInteractor: PresentableInteractor<MyPageModifyInfoPr
 
     override init(presenter: MyPageModifyInfoPresentable) {
         super.init(presenter: presenter)
+        presenter.handler = self
     }
 
     override func didBecomeActive() {
         super.didBecomeActive()
+        bind()
     }
 
     override func willResignActive() {
         super.willResignActive()
     }
+
+    private func bind() {
+        bindAction()
+    }
+
+    private func bindAction() {
+        guard let action = presenter.action else { return }
+        action.didTapBackButton
+            .withUnretained(self)
+            .bind(onNext: { owner, _ in
+            owner.listener?.popModifyInfoView()
+        }).disposeOnDeactivate(interactor: self)
+    }
+}
+
+extension MyPageModifyInfoInteractor: MyPageModifyInfoPresenterHandler {
+
 }
