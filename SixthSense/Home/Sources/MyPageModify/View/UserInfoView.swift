fae1973590b276b1dfaeffd65f18b950318fd700
AllieKim <allie0147@gmail.com>
2022-09-10 12:21:52 +0900

    Feat : 마이페이지 상세 화면 stack에 들어가는 subview 구현

--- SixthSense/Home/Sources/MyPageModify/View/UserInfoView.swift
+++ SixthSense/Home/Sources/MyPageModify/View/UserInfoView.swift
@@ -9,8 +9,29 @@
 import UIKit
 import Then
 import SnapKit
+import DesignSystem
 
 final class UserInfoview: UIView {
+
+    var titleLabel = UILabel().then {
+        $0.textColor = AppColor.systemGray500
+        $0.font = AppFont.body2
+    }
+
+    var userDataLabel = UILabel().then {
+        $0.textColor = AppColor.systemBlack
+        $0.font = AppFont.body2
+    }
+
+    private var editButton = UIButton().then {
+        $0.layer.do {
+            $0.backgroundColor = AppColor.systemGray100.cgColor
+            $0.cornerRadius = 12
+        }
+        $0.setAttributedTitle(NSAttributedString(string: "수정",
+                                                 attributes: [.font: AppFont.caption, .foregroundColor: AppColor.systemGray700]),
+                              for: .normal)
+    }
 
 
     override init(frame: CGRect) {
@@ -24,10 +45,29 @@ final class UserInfoview: UIView {
     }
 
     private func configureUI() {
-
+        addSubviews(titleLabel, userDataLabel, editButton)
     }
 
     private func configureLayout() {
+        self.snp.makeConstraints {
+            $0.height.equalTo(58)
+        }
 
+        titleLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.left.equalToSuperview().inset(20)
+        }
+
+        userDataLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.left.equalTo(titleLabel.snp.right).offset(34)
+        }
+
+        editButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.right.equalToSuperview().inset(20)
+            $0.height.equalTo(24)
+            $0.width.equalTo(45)
+        }
     }
 }
