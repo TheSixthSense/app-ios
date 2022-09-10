51b287fbf2a6a5f27c14e72facdb6f6cfc012b3a
AllieKim <allie0147@gmail.com>
2022-09-10 12:41:24 +0900

    Feat : userPayload 작성 및 필요한 enum 작성

---
+++ SixthSense/Home/Sources/MyPage/Domain/Enums/GenderInfo.swift
@@ -0,0 +1,31 @@
+//
+//  GenderInfo.swift
+//  Home
+//
+//  Created by Allie Kim on 2022/09/10.
+//  Copyright © 2022 kr.co.thesixthsense. All rights reserved.
+//
+
+import Foundation
+
+public enum GenderInfo: String {
+
+    case male = "MALE"
+    case female = "FEMALE"
+    case etc = "ETC"
+    case none = "NONE"
+
+    var localized: String {
+        switch self {
+        case .male:
+            return "남성"
+        case .female:
+            return "여성"
+        case .etc:
+            return "그 외 성별"
+        case .none:
+            return "선택하지 않음"
+        }
+    }
+}
+
