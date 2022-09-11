d14394190f0f14d5739ea7d5740e725d5b7bc8f0
Moon Hyo Jae <followmoonrise@gmail.com>
2022-09-11 14:44:48 +0900

    feat : 캘린더 | Usecase를 스펙을 정의하고 API를 붙혔어요

---
+++ SixthSense/Home/Sources/ChallengeCalendar/ChallengeCalendarUseCase.swift
@@ -0,0 +1,61 @@
+//
+//  ChallengeCalendarUseCase.swift
+//  Home
+//
+//  Created by 문효재 on 2022/09/09.
+//  Copyright © 2022 kr.co.thesixthsense. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import Repository
+
+typealias DayState = ((Date) -> ChallengeCalendarDayState)
+
+protocol ChallengeCalendarUseCase {
+    func fetch(by date: Date) -> Observable<DayState>
+}
+
+final class ChallengeCalendarUsCaseImpl: ChallengeCalendarUseCase {
+    private let repository: UserChallengeRepository
+    
+    init(repository: UserChallengeRepository) {
+        self.repository = repository
+    }
+    
+    func fetch(by date: Date) -> Observable<DayState> {
+        return repository.monthList(by: date.toString(dateFormat: "yyyy-MM-dd"))
+            .asObservable()
+            .compactMap { UserChallengeCalendarList(JSONString: $0) }
+            .map(\.data)
+            .withUnretained(self)
+            .flatMap { owner, response -> Observable<DayState> in
+                owner.configureDayStates(response)
+            }
+    }
+    
+    private func configureDayStates(_ items: [UserChallengCalendarItem]) -> Observable<DayState> {
+        let challenges = items.compactMap { DayChallenge(date: $0.date,
+                                                         successCount: $0.successCount,
+                                                         totalCount: $0.totalCount)}
+
+        return .just { date in
+            challenges.filter {
+                $0.date == date.toString(dateFormat: "yyyy-MM-dd")
+            }
+            .compactMap(\.state)
+            .last ?? .none
+        }
+    }
+    
+    private struct DayChallenge {
+        let date: String
+        let state: ChallengeCalendarDayState?
+        
+        init?(date: String, successCount: Int, totalCount: Int) {
+            guard totalCount != 0 else { return nil }
+            self.date = date
+            self.state = ChallengeCalendarDayState(percentage: Double(successCount) / Double(totalCount), date: date)
+        }
+    }
+}
