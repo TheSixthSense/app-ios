f6eba85f7fd3548157fe3f21ad52f0ced9732240
AllieKim <allie0147@gmail.com>
2022-09-11 19:50:54 +0900

    Feat : 마이페이지 usecase에 토큰 정보 추가

--- SixthSense/Home/Sources/MyPage/Domain/UseCases/MyPageUseCase.swift
+++ SixthSense/Home/Sources/MyPage/Domain/UseCases/MyPageUseCase.swift
@@ -8,20 +8,24 @@
 
 import Foundation
 import RxSwift
+import Storage
 import Repository
 
 public protocol MyPageUseCase {
     func fetchUserData() -> Observable<UserInfoModel>
     func fetchUserChallengeStats() -> Observable<UserChallengeStatModel>
     func logout() -> Observable<Void>
+    func isLoggedIn() -> Observable<Bool>
 
 }
 final class MyPageUseCaseImpl: MyPageUseCase {
 
     private let userRepository: UserRepository
+    private let persistence: LocalPersistence
 
-    init(userRepository: UserRepository) {
+    init(userRepository: UserRepository, persistence: LocalPersistence) {
         self.userRepository = userRepository
+        self.persistence = persistence
     }
 
     func fetchUserData() -> Observable<UserInfoModel> {
@@ -36,8 +40,14 @@ final class MyPageUseCaseImpl: MyPageUseCase {
             .asObservable()
     }
 
+    func isLoggedIn() -> Observable<Bool> {
+        guard let token: String = persistence.value(on: .accessToken) else { return .just(false) }
+        return .just(!token.isEmpty)
+    }
+
     func logout() -> Observable<Void> {
         return userRepository.logout()
+            .do(onSuccess: { [weak self] _ in self?.persistence.delete(on: .accessToken) })
             .flatMap({ _ in .just(()) })
             .asObservable()
     }
